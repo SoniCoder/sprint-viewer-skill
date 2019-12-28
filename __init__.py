9277c1bbaa97429851e561cc1a3b3020d16f6790
Hritik Soni <hritik.s@greyorange.com>
2019-12-28 12:23:14 +0000

    ported node js version to python

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from datetime import datetime
+from datetime import timedelta
+import pytz
+import math
 
 class SprintViewer(MycroftSkill):
     def __init__(self):
@@ -7,7 +10,12 @@ class SprintViewer(MycroftSkill):
 
     @intent_file_handler('viewer.sprint.intent')
     def handle_viewer_sprint(self, message):
-        self.speak_dialog('viewer.sprint')
+        base_sprint = 78
+        base_sprint_date = datetime(2019, 4, 1, 0, 0, 0)
+        cur_date = datetime.now(pytz.utc) + timedelta(minutes=30, hours=5)
+        months_passed = cur_date.month - base_sprint_date.month
+        current_sprint = base_sprint + months_passed*2 + math.floor((cur_date.day)/16)
+        self.speak("this is sprint %d" % current_sprint)
 
 
 def create_skill():
